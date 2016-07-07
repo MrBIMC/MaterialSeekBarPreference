22896111d868f785019abf090711cf0927b7e961
Nicholas Winder <ntwinder@gmail.com>
2016-07-08 08:12:38 +1200

    Enabled the usage of the seekbar in a navigation drawer.

--- library/src/main/java/com/pavelsikun/seekbarpreference/PreferenceControllerDelegate.java
+++ library/src/main/java/com/pavelsikun/seekbarpreference/PreferenceControllerDelegate.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
@@ -137,6 +138,31 @@ class PreferenceControllerDelegate implements SeekBar.OnSeekBarChangeListener, V
 
         setMaxValue(maxValue);
         seekBarView.setOnSeekBarChangeListener(this);
+        // This enables the seekbar to catch drag left when in a navigation drawer
+        seekBarView.setOnTouchListener(new SeekBar.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                int action = event.getAction();
+                switch (action)
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        // Disallow Drawer to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        // Allow Drawer to intercept touch events.
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+
+                // Handle seekbar touch events.
+                v.onTouchEvent(event);
+                return true;
+            }
+        });
 
         measurementView.setText(measurementUnit);
 
